b221e56d5b1fff9098927d17af21471856332892
Rebecca Tong <rtong@buffalo.edu>
2018-07-24 00:33:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,6 +50,15 @@ class ApplicationController < Sinatra::Base
 		end
   end
 
+  get "/success" do
+		if logged_in?
+			erb :success
+		else
+			redirect "/login"
+		end
+	end
+
+
   get "/failure" do
     erb :failure
   end
